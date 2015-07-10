f631664252cc7e1c67431d6c60ab6f96c2165727
Simon Beal <sb48@sanger.ac.uk>
2015-07-10 14:22:36 +0100

    In out_check.py, copies results of samples to dest dir The cleanup step is more verbose Update .gitignore Remove sample_names.txt

--- out_check.py
+++ out_check.py
@@ -1,4 +1,4 @@
-import json, os, functools, glob
+import json, os, functools, glob, shutil
 
 CCLE_DIR = "/lustre/scratch112/sanger/cgppipe/cttv-rnaseq-am26/ccle-fusions/"
 #Files that need to be present for the job to be considered 'sucessfully finished'
@@ -14,6 +14,10 @@ COMPLETED_FILES = ["logs_star",
 #Files that are only present if the job is still running or failed
 TMP_FILES = ["tmpTophatFusion",
              "tmpStar"]
+#The destination for copied results files once finished
+DEST_DIR = "/nfs/team78pc20/cttv-rnaseq-am26/ccle-fusions/"
+#File extentions that should be copied
+cp_extentions = ["html", "txt"]
 
 class Main(object):
     def __init__(self):
@@ -38,25 +42,40 @@ class Main(object):
 
     def cleanup(self):
         deleted = 0
+        copied = 0
         master = open(os.path.join(CCLE_DIR, "tracking", "cell-lines-tracking-MASTER.txt"), "r+")
         for sample_name, complete in zip(self.sample_names, self.samples_complete):
             if complete:
-                master.seek(0)
-                while 1:
-                    line = master.readline()
-                    if line.split("\t")[0] == sample_name:
-                        master.seek(-5, 1)
-                        master.write("DONE")
-                    if line == "": break
-                deleted += 1
-                data_dir = os.path.join(self.batch_path, sample_name, "data")
-                for f in glob.glob(os.path.join(data_dir, "*")):
-                    #print f
-                    os.remove(f)
-                else:
-                    deleted -= 1
+                copied+=self.copy_results(sample_name)
+                self.update_spreadsheet(master, sample_name)
+                if self.cleanup_delete(sample_name):
+                    deleted += 1
         master.close()
-        print "In the cleanup step, %d samples were deleted"%deleted
+        print "In the cleanup step, %d samples were deleted, %d samples had their spreadsheet data updated and %d files were copied"%(deleted, sum(self.samples_complete), copied)
+
+    def cleanup_delete(self, sample_name):
+        data_dir = os.path.join(self.batch_path, sample_name, "data")
+        for f in glob.glob(os.path.join(data_dir, "*")):
+            os.remove(f)
+        else: return False
+        return True
+
+    def update_spreadsheet(self, master, sample_name):
+        master.seek(0)
+        while 1:
+            line = master.readline()
+            if line.split("\t")[0] == sample_name:
+                master.seek(-5, 1)
+                master.write("DONE")
+            if line == "": break
+
+    def copy_results(self, sample_name):
+        files = []
+        for ext in cp_extentions:
+            files.extend(glob.glob(os.path.join(self.batch_path, sample_name, "results", "*.%s"%ext)))
+        for f in files:
+            shutil.copy2(f, os.path.join(DEST_DIR, os.path.basename(f)))
+        return len(files)
 
     def check_sample_files(self, file_list, sample):
         results_path = os.path.join(self.batch_path, sample, "results")
